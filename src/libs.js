b504b89525fc8b8db182912739440df00b36e5f0
Ymlluo <ymlluo@gmail.com>
2023-04-10 18:47:17 +0800

    libs add jsdoc

--- src/libs.js
+++ src/libs.js
@@ -1,31 +1,45 @@
 import axios from "axios";
 
+/**
+ * Determines whether the given text is a URL or a path.
+ * @param {string} text - The text to check.
+ * @returns {boolean} - True if the text is a URL or a path, false otherwise.
+ */
 export function isUrlOrPath(text) {
-  // 匹配以http、https、ftp、ftps、file、data、mailto、tel、sms、urn开头的字符串，或者以/或./开头的字符串
-  var regex = /^(http|https|ftp|ftps|file|data|mailto|tel|sms|urn):\/\/[\S]+|^\/[\S]+|^\.\/[\S]+/;
+  const regex = /^(http|https|ftp|ftps|file|data|mailto|tel|sms|urn):\/\/[\S]+|^\/[\S]+|^\.\/[\S]+/;
   return regex.test(text);
 }
 
 /**
- *
- * @param {*} element
- * @returns
+ * Returns the target element.
+ * @param {Element|string} element - The element or selector string.
+ * @returns {Element} - The target element.
  */
 export function getTargetElement(element) {
   if (element instanceof Element) {
     return element;
-  }else {
+  } else {
     return document.querySelector(element);
   }
 }
 
+/**
+ * Makes the given element resizable.
+ * @param {HTMLElement} el - The element to make resizable.
+ */
 export function makeResizable(el) {
-  var element = getTargetElement(el);
-  element.style.position = "absolute";
-  element.style.resize = "both";
-  element.style.overflow = "auto";
+  const element = getTargetElement(el);
+  if (element) {
+    element.style.position = "absolute";
+    element.style.resize = "both";
+    element.style.overflow = "auto";
+  }
 }
-
+/**
+ * Makes an element draggable
+ * @param {HTMLElement} elmnt - The element to be made draggable
+ * @param {HTMLElement} handler - The element to be used as the handler for dragging
+ */
 export function makeDraggable(elmnt, handler) {
   elmnt = getTargetElement(elmnt);
   handler = getTargetElement(handler);
@@ -47,6 +61,10 @@ export function makeDraggable(elmnt, handler) {
     elmnt.onmousedown = dragMouseDown;
   }
 
+  /**
+   * Function called when the mouse is pressed down on the draggable element
+   * @param {MouseEvent} e - The mouse event
+   */
   function dragMouseDown(e) {
     // Prevent any default action on this element (you can remove if you need this element to perform its default action)
     e.preventDefault();
@@ -59,6 +77,10 @@ export function makeDraggable(elmnt, handler) {
     document.onmousemove = elementDrag;
   }
 
+  /**
+   * Function called when the draggable element is being dragged
+   * @param {MouseEvent} e - The mouse event
+   */
   function elementDrag(e) {
     // Prevent any default action on this element (you can remove if you need this element to perform its default action)
     e.preventDefault();
@@ -74,6 +96,10 @@ export function makeDraggable(elmnt, handler) {
     // console.error(currentPosX, currentPosY, previousPosX, previousPosY);
   }
 
+  /**
+   * Function called when the mouse button is released, stopping the dragging of the element
+   * @param {MouseEvent} e - The mouse event
+   */
   function closeDragElement(e) {
     // Stop moving when mouse button is released and release events
     document.onmouseup = null;
@@ -94,6 +120,13 @@ export function makeDraggable(elmnt, handler) {
   }
 }
 
+
+/**
+ * Calculates the dimensions of the overlap between two divs.
+ * @param {HTMLElement} div1 - The first div element.
+ * @param {HTMLElement} div2 - The second div element.
+ * @returns {Object} An object containing the width and height of the overlap.
+ */
 export function getOverlapDimensions(div1, div2) {
   const rect1 = div1.getBoundingClientRect();
   const rect2 = div2.getBoundingClientRect();
@@ -109,64 +142,21 @@ export function getOverlapDimensions(div1, div2) {
   return { overlapWidth, overlapHeight };
 }
 
-export async function getUrl(url) {
-  try {
-    if (typeof axios === "function") {
-      try {
-        const response = await axios.get(url);
-        return response.data;
-      } catch (error) {
-        console.error(error);
-        throw error; // re-throw error to propagate it to the caller
-      }
-    }
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", "url", false); // false表示使用同步请求
-    xhr.send();
-    if (xhr.status === 200) {
-      return xhr.responseText;
-    } else {
-      console.error("请求失败：" + xhr.status);
-    }
-  } catch (error) {
-    console.error(error);
-    return "";
-  }
-}
 
-export async function postUrl(url, data) {
-  try {
-    if (typeof axios === "function") {
-      const response = await axios.post(url, data);
-      return response.data;
-    }
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
 
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 201) {
-        return xhr.responseText;
-      } else {
-        console.error("请求失败：" + xhr.status);
-      }
-    };
-    xhr.send(data);
-    if (xhr.status === 200) {
-      return xhr.responseText;
-    } else {
-      console.error("请求失败：" + xhr.status);
-    }
-  } catch (error) {
-    console.error(error);
-    return error;
-  }
-}
-
+/**
+ * Generates a unique dialog ID.
+ * @returns {string} The generated dialog ID.
+ */
 export function genDialogId() {
   const modalId = "dialg-" + Math.floor(Math.random() * 1000000);
   return modalId;
 }
 
+/**
+ * Creates a modal wrapper element with specific classes and attributes.
+ * @returns {HTMLElement} The modal wrapper element.
+ */
 export function setModalWrapper() {
   let modal = document.createElement("div");
   modal.classList.add("modal", "bs5dialog-modal", "fade");
@@ -177,9 +167,10 @@ export function setModalWrapper() {
 }
 
 /**
- * add disabled class ，default lock 1s timeout
- * @param {*} element
- * @param {*} timeout
+ * Locks the replay button for a specified amount of time.
+ * @param {HTMLElement} element - The replay button element.
+ * @param {number} [timeout=1000] - The amount of time to lock the button in milliseconds.
+ * @returns {number} - The timer ID.
  */
 export function replayLock(element, timeout = 1000) {
   const targetElement = getTargetElement(element);
@@ -192,6 +183,10 @@ export function replayLock(element, timeout = 1000) {
   return timer;
 }
 
+/**
+ * Returns the maximum z-index value of all elements in the document.
+ * @returns {number} The maximum z-index value.
+ */
 export function getMaxZIndex() {
   let maxZIndex = 0;
   document.querySelectorAll("*").forEach(element => {
@@ -202,7 +197,11 @@ export function getMaxZIndex() {
   });
   return maxZIndex;
 }
-
+/**
+ * Returns the appropriate text color class based on the perceived brightness of a background color.
+ * @param {string} bgColorClassName - The class name of the background color element.
+ * @returns {string} - The class name of the appropriate text color.
+ */
 export function getTextClass(bgColorClassName) {
   if (bgColorClassName === "bg-link") {
     return "text-black";
@@ -238,22 +237,24 @@ export function getTextClass(bgColorClassName) {
   return perceivedBrightness > 150 ? "text-black" : "text-white";
 }
 
-export async function makeRequest(url, method = 'GET', headers = {}, body = null) {
+
+
+export async function makeRequest(url, method = "GET", headers = {}, body = null) {
   const csrfTokenMeta = document.querySelector('meta[name="csrf-token"]');
   if (csrfTokenMeta) {
-    headers['X-CSRF-TOKEN'] = csrfTokenMeta.content;
+    headers["X-CSRF-TOKEN"] = csrfTokenMeta.content;
   }
 
-  if (typeof axios !== 'undefined') {
-    axios.defaults.crossDomain=true
-    axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+  if (typeof axios !== "undefined") {
+    axios.defaults.crossDomain = true;
+    axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
     // Use axios if it's available
     try {
       const axiosConfig = {
         url,
         method,
         headers,
-        withCredentials: false, // Allow cookies to be sent and received for cross-domain requests
+        withCredentials: false // Allow cookies to be sent and received for cross-domain requests
       };
 
       if (body) {
@@ -263,7 +264,7 @@ export async function makeRequest(url, method = 'GET', headers = {}, body = null
         } else {
           // If body is not a FormData object, assume it's JSON data
           axiosConfig.data = JSON.stringify(body);
-          axiosConfig.headers['Content-Type'] = 'application/json';
+          axiosConfig.headers["Content-Type"] = "application/json";
         }
       }
 
@@ -271,14 +272,13 @@ export async function makeRequest(url, method = 'GET', headers = {}, body = null
       return {
         isSuccess: true,
         statusCode: response.status,
-        content: response.data,
+        content: response.data
       };
     } catch (error) {
-      
       return {
         isSuccess: false,
         statusCode: error.response?.status,
-        content: error.response?.data || error.message,
+        content: error.response?.data || error.message
       };
     }
   } else {
@@ -287,9 +287,9 @@ export async function makeRequest(url, method = 'GET', headers = {}, body = null
       method,
       headers: {
         ...headers,
-        'Accept': 'application/json',
+        Accept: "application/json"
       },
-      credentials: 'include', // Allow cookies to be sent and received for cross-domain requests
+      credentials: "include" // Allow cookies to be sent and received for cross-domain requests
     };
 
     if (body) {
@@ -299,7 +299,7 @@ export async function makeRequest(url, method = 'GET', headers = {}, body = null
       } else {
         // If body is not a FormData object, assume it's JSON data
         options.body = JSON.stringify(body);
-        options.headers['Content-Type'] = 'application/json';
+        options.headers["Content-Type"] = "application/json";
       }
     }
 
@@ -309,19 +309,54 @@ export async function makeRequest(url, method = 'GET', headers = {}, body = null
       return {
         isSuccess: response.ok,
         statusCode: response.status,
-        content: data,
+        content: data
       };
     } catch (error) {
       return {
         isSuccess: false,
         statusCode: error.status,
-        content: error.message,
+        content: error.message
       };
     }
   }
 }
 
+/**
+ * Creates a custom event with the given name and detail
+ * @param {string} eventName - The name of the event
+ * @param {object} detail - The detail object to be attached to the event
+ * @returns {CustomEvent|Event} - The created event
+ */
+export function makeEvent(eventName, detail) {
+  const params = {
+    bubbles: true,
+    cancelable: true,
+    detail: detail
+  };
+  if (typeof window.CustomEvent === "function") {
+    return new CustomEvent(eventName, params);
+  } else {
+    // IE 11 support
+    // https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+    const evt = document.createEvent("CustomEvent");
+    evt.initCustomEvent(eventName, params.bubbles, params.cancelable, params.detail);
+    return evt;
+  }
+}
 
+/**
+ * Triggers a custom event with the given name and detail on the given element
+ * @param {HTMLElement} element - The element on which to trigger the event
+ * @param {string} eventName - The name of the event to trigger
+ * @param {object} detail - The detail object to be attached to the event
+ */
+export function triggerEvent(element, eventName = "", detail = {}) {
+  if (!element || !eventName) {
+    return;
+  }
+  const event = makeEvent(eventName, detail);
+  element.dispatchEvent(event);
+}
 
 export default {
   getTargetElement,
@@ -329,12 +364,12 @@ export default {
   makeDraggable,
   makeResizable,
   makeRequest,
-  getUrl,
-  postUrl,
   getOverlapDimensions,
   genDialogId,
   setModalWrapper,
   replayLock,
   getMaxZIndex,
-  getTextClass
+  getTextClass,
+  makeEvent,
+  triggerEvent
 };
