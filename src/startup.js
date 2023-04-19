3281ad8f377e00e15e17f3c0179665041fa0fe03
Ymlluo <ymlluo@gmail.com>
2023-04-19 18:33:37 +0800

    update startup

--- src/startup.js
+++ src/startup.js
@@ -8,30 +8,54 @@ import { toast } from "./components/toast";
 import { setSystemLang } from "./i18n";
 
 const components = { alert, confirm, prompt, message, toast, load, offcanvas };
+/**
+ * Returns an object containing all the data-bs5-dialog-option-* attributes of an element
+ * @param {HTMLElement} elem - The element to get the options from
+ * @returns {Object} - An object containing all the options
+ */
+function getDialogOptions(elem) {
+  return Object.fromEntries(
+    [...elem.attributes]
+      .filter(attr => attr.name.startsWith("data-bs5-dialog-option-"))
+      .map(attr => [attr.name.replace("data-bs5-dialog-option-", ""), attr.value])
+  );
+}
+
+/**
+ * Shows a dialog based on the data-bs5-dialog attribute of an element
+ * @param {HTMLElement} elem - The element to show the dialog for
+ */
+function showDialog(elem) {
+  const content = elem.title || elem.dataset.content || "";
+  const elemOpts = elem.dataset.bs5DialogOptions ? JSON.parse(elem.dataset.bs5DialogOptions) : {};
+  const func = components[elem.dataset.bs5Dialog];
+  if (typeof func === "function") {
+    func(content.trim(), { ...getDialogOptions(elem), ...elemOpts });
+  } else {
+    console.log(`Function ${elem.dataset.bs5Dialog} does not exist`);
+  }
+}
+
+/**
+ * Adds click listeners to all elements with the data-bs5-dialog attribute
+ */
 export function addDialogClickListeners() {
   const dialogElems = document.querySelectorAll("[data-bs5-dialog]");
   for (const elem of dialogElems) {
-    const opts = Object.fromEntries(
-      [...elem.attributes]
-        .filter(attr => attr.name.startsWith("data-bs5-dialog-option-"))
-        .map(attr => [attr.name.replace("data-bs5-dialog-option-", ""), attr.value])
-    );
     elem.addEventListener("click", () => {
-      const content = elem.title || elem.dataset.content || "";
-      const elemOpts = elem.dataset.bs5DialogOptions ? JSON.parse(elem.dataset.bs5DialogOptions) : {};
-      const func = components[elem.dataset.bs5Dialog];
-      if (typeof func === "function") {
-        func(content.trim(), { ...opts, ...elemOpts });
-      } else {
-        console.log(`Function ${elem.dataset.bs5Dialog} does not exist`);
-      }
+      showDialog(elem);
     });
   }
 }
 
+/**
+ * Initializes the dialog system by setting the system language and adding click listeners to all elements with the data-bs5-dialog attribute
+ */
 export function startup() {
   setSystemLang();
   addDialogClickListeners();
 }
 
 export default startup;
+
+
